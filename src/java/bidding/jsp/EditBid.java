9c5aa850f87d92d9f4bb2dc1091b77237a093f27
sedog <sedog@SFS-PC-02.sfs.uncc.edu>
2015-04-08 17:32:52 -0400

    altered import statements for new jar structure.

---
+++ src/java/bidding/jsp/EditBid.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bidding.jsp;
+
+/**
+ * Contains the names for any request input parameters or session attributes for
+ * the PlaceBid and UpdateBid use cases, as the same information is used.
+ * 
+ * @author sedog
+ */
+public enum EditBid {
+    // Request input parameter for the bid drop off time
+    PARA_DROP_OFF_TIME("drop_off_time"),
+    // Request input parameter for the bid pick up time
+    PARA_PICKUP_TIME("pickup_time"),
+    // Request input parameter for the bid fee
+    PARA_FEE("fee");
+    
+    public final String name;
+    
+    private EditBid (String name) {
+        this.name = name;
+    }
+}
